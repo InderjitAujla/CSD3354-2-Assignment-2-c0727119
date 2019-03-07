c786acda099ffc6b9492feb943056cb0d6f4df89
visualstudio <visualstudio@230-PC40>
2019-03-07 14:31:13 -0500

    lab 1

--- CSD3354-2-Assignemt-2-c0727119/Program.cs
+++ CSD3354-2-Assignemt-2-c0727119/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 
 namespace CSD3354_2_Assignemt_2_c0727119
+//Sonia Manhas c0726358
+//Inderjit Sngh Aujla c0727119
 {
-    public class DelegateExercises
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class DelegateExcercises
     {
         public delegate void MyDelegate();
-        void Methodl()
+
+        void Method1()
         {
             Console.WriteLine("Method1");
             Console.ReadLine();
         }
-
         public void Method2()
         {
-            MyDelegate myDelegate = new MyDelegate(Methodl);
-            myDelegate();
+            MyDelegate mydelegate = new MyDelegate(Method1);
+            mydelegate();
         }
     }
 }
